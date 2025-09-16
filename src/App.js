e4b202bd75e438d682161bce0e9bb99ac8a37b4d
Aryan Choudhary <choudharyaryan855@gmail.com>
2025-09-16 22:16:05 +0530

    Fix final API URLs

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
   useEffect(() => {
     const fetchMappings = async () => {
       try {
-        const response = await axios.get("http://127.0.0.1:5000/get_data_mappings");
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/get_data_mappings`);
         setMappings(response.data);
         console.log("Successfully fetched data mappings from backend.", response.data);
       } catch (err) {
@@ -115,7 +115,7 @@ function App() {
         ...oneHotOwner,
       };
 
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/predict`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
@@ -188,7 +188,7 @@ function App() {
               <select name="location" value={formData.location} onChange={handleChange} required>
                 <option value="">Select location</option>
                 {Object.entries(mappings.locations).map(([locName, locCode]) => (
-                    <option key={locCode} value={locCode}>{locName}</option>
+                  <option key={locCode} value={locCode}>{locName}</option>
                 ))}
                 <option value="-1">Other</option> 
               </select>
