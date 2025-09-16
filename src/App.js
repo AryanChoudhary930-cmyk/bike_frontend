5e1c9646db0fe202bb8cb2c23410d81b33aea232
aryan choudhary <157775349+AryanChoudhary930-cmyk@users.noreply.github.com>
2025-09-16 22:11:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -115,7 +115,7 @@ function App() {
         ...oneHotOwner,
       };
 
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/predict`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
@@ -188,7 +188,7 @@ function App() {
               <select name="location" value={formData.location} onChange={handleChange} required>
                 <option value="">Select location</option>
                 {Object.entries(mappings.locations).map(([locName, locCode]) => (
-                    <option key={locCode} value={locCode}>{locName}</option>
+                  <option key={locCode} value={locCode}>{locName}</option>
                 ))}
                 <option value="-1">Other</option> 
               </select>
